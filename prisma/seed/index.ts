85e797f4384e12d24f09346265a7d11a27876955
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-04-12 18:16:46 -0300

    datamodel & populate database (#7)

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,13 +1,15 @@
-import { faker } from '@faker-js/faker'
 import { PrismaClient } from '@prisma/client'
+import {
+  createCategory,
+  createCompany,
+  createContactInfo,
+  createRole,
+  createSkill,
+  createSocialInfo,
+  createTalent,
+} from './utils'
 
 const prisma = new PrismaClient()
-
-function createCategory() {
-  return {
-    name: faker.name.jobArea(),
-  }
-}
 
 async function main() {
   console.log('Seeding database')
@@ -17,18 +19,93 @@ async function main() {
   await prisma.category.deleteMany({ where: {} })
   await prisma.company.deleteMany({ where: {} })
   await prisma.skill.deleteMany({ where: {} })
+  await prisma.role.deleteMany({ where: {} })
   await prisma.job.deleteMany({ where: {} })
   console.timeEnd('Clear database')
 
-  console.time('Inserting categories')
+  console.time('Inserting skills')
+  const skills = await Promise.all(
+    Array.from({ length: 30 }, async () => {
+      return await prisma.skill.create({
+        data: { ...createSkill() },
+      })
+    }),
+  )
+  console.timeEnd('Inserting skills')
+
+  console.time('Inserting roles')
+  const roles = await Promise.all(
+    Array.from({ length: 30 }, async () => {
+      return await prisma.role.create({
+        data: { ...createRole() },
+      })
+    }),
+  )
+
+  console.timeEnd('Inserting roles')
+
+  console.time('Inserting talents')
+  const skillIds = skills.map(s => s.id)
+  const rolesIds = roles.map(r => r.id)
   Promise.all(
-    Array.from({ length: 5 }, async () => {
-      await prisma.category.create({
+    Array.from({ length: 50 }, async () => {
+      await prisma.talent.create({
+        data: {
+          ...createTalent(),
+          role: {
+            connect: {
+              id: rolesIds[Math.floor(Math.random() * rolesIds.length)],
+            },
+          },
+          skills: {
+            connect: {
+              id: skillIds[Math.floor(Math.random() * skillIds.length)],
+            },
+          },
+          socialInfo: {
+            create: {
+              ...createSocialInfo(),
+            },
+          },
+          contactInfo: {
+            create: {
+              ...createContactInfo(),
+            },
+          },
+        },
+      })
+    }),
+  )
+
+  console.timeEnd('Inserting talents')
+
+  console.time('Inserting categories')
+  const categories = await Promise.all(
+    Array.from({ length: 20 }, async () => {
+      return await prisma.category.create({
         data: { ...createCategory() },
       })
     }),
   )
   console.timeEnd('Inserting categories')
+
+  console.time('Inserting companies')
+  const categoryIds = categories.map(c => c.id)
+  Promise.all(
+    Array.from({ length: 20 }, async () => {
+      await prisma.company.create({
+        data: {
+          category: {
+            connect: {
+              id: categoryIds[Math.floor(Math.random() * categoryIds.length)],
+            },
+          },
+          ...createCompany(),
+        },
+      })
+    }),
+  )
+  console.timeEnd('Inserting companies')
 }
 
 main()
