9f2fea63c75af1ae362439955e209379d457a2fa
roblesdotdev <httpsnip@gmail.com>
2023-04-11 21:44:17 -0300

    prettier config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="font-bold text-2xl">Welcome to Empli</h1>
     </main>
-  );
+  )
 }
