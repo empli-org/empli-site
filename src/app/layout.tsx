66471518e07a79d7a1b57318cba3c34c7bf3d040
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-04-11 21:59:28 -0300

    chore: Initial cfg (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import "./globals.css";
+import './globals.css'
 
 export const metadata = {
-  title: "Empli Website",
-  description: "Empli Website",
-};
+  title: 'Empli Website',
+  description: 'Empli Website',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
