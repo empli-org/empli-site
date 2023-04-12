bb4743b411face7bf80400744eaa709984e08710
roblesdotdev <httpsnip@gmail.com>
2023-04-11 21:46:21 -0300

    check tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="font-bold text-2xl">Welcome to Empli</h1>
+      <h1 className="text-2xl font-bold">Welcome to Empli</h1>
     </main>
   )
 }
