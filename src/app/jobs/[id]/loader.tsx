e1c8b4edf309435264e24b55e99d88cd21dea0ba
roblesdotdev <httpsnip@gmail.com>
2023-04-12 23:42:09 -0300

    feat: scaffold jobs page

---
+++ src/app/jobs/[id]/loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="text-lg font-bold">
+      <p>Loading...</p>
+    </div>
+  )
+}
--- src/lib/jobs.server.ts
+++ src/lib/jobs.server.ts
@@ -1,11 +1,27 @@
-export async function fetchCategories() {
+export async function getJobListing({
+  query,
+  limit,
+}: {
+  query?: string
+  limit?: number
+}) {
   try {
-    const response = await fetch('http://localhost:3000/api/categories')
-
-    if (!response.ok) throw new Error('Fail to fetch categories')
-
+    const response = await fetch(
+      `http://127.0.0.1:3000/api/jobs?query=${query}&limit=${limit}`,
+    )
+    if (!response.ok) throw new Error('Fail to fetch jobs')
     return response.json()
   } catch {
     return []
+  }
+}
+
+export async function getJobById(id: string) {
+  try {
+    const response = await fetch(`http://127.0.0.1:3000/api/jobs/${id}`)
+    if (!response.ok) throw new Error('Fail to fetch job by id')
+    return response.json()
+  } catch {
+    return null
   }
 }
